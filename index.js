322970c54c6d9b12c612d70607f3687b4244e670
Jin <88888888@qq.com>
2021-09-30 01:34:35 +0800

    实现双指后撤

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ new Vue({
             sizeIndex:1,
             touch:false,
             size:1,
-            lastPoint:{x:undefined,y:undefined}
+            lastPoint:{x:undefined,y:undefined},
+            canvas:null,
+            ctx:null,
+            statusImg:null
         }
     },
     mounted() {
@@ -22,58 +25,55 @@ new Vue({
             this.canvasHeight = this.$refs.canvasCantainer.clientHeight
             canvas.width = this.canvasWidth
             canvas.height = this.canvasHeight
+            // 开启2d的绘制环境
+            ctx = canvas.getContext("2d");
+            //设置线条颜色
+            ctx.strokeStyle  = this.color
+            //设置线条端头样式
+            ctx.lineCap='round'
+            this.canvas = canvas
+            this.ctx = ctx
         },
         chooseColor(color){
             // console.log(111);
             this.color=color
+            this.ctx.strokeStyle  = this.color
         },
         changeSizeIndex(sizeIndex){
             this.sizeIndex=sizeIndex
             this.size=sizeIndex
         },
         drawStart(event){
-            const canvas = this.$refs.canvas
-            // 开启2d的绘制环境
-            ctx = canvas.getContext("2d");
             if(event.touches.length===1){
-                ctx.save()
+                this.statusImg = this.ctx.getImageData(0,0,this.canvasWidth,this.canvasHeight)
+                return
             }
             // console.log(event.touches.length===2);
             if(event.touches.length===2){
-                ctx.restore()
+                this.ctx.putImageData(this.statusImg,0,0)
+                // this.touch=true
+                return
             }
-            // this.touch=true
-            // console.log(this.touch)
         },
         drawEnd(){
             // this.touch=false
             // // console.log(this.touch)
-            // this.lastPoint={x:undefined,y:undefined}
+            this.lastPoint={x:undefined,y:undefined}
         },
         drawCanvas(event){
             // 当手指滑动时
-            // 获取canvas
-            const canvas = this.$refs.canvas
-            // 开启2d的绘制环境
-            ctx = canvas.getContext("2d");
             // 绘制线条
             let newPoint= {x:event.targetTouches[0].pageX, y:event.targetTouches[0].pageY}
-            ctx.beginPath();
-            ctx.moveTo(this.lastPoint.x, this.lastPoint.y);
-            ctx.lineWidth = this.size*1.5
-            ctx.strokeStyle  = this.color;
-            ctx.lineCap='round'
-            ctx.lineTo(newPoint.x, newPoint.y);
-            ctx.stroke();
-            ctx.closePath();
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.lastPoint.x, this.lastPoint.y);
+            this.ctx.lineWidth = this.size*1.5
+            this.ctx.lineTo(newPoint.x, newPoint.y);
+            this.ctx.stroke();
+            this.ctx.closePath();
             this.lastPoint = newPoint
         },
         clear(){
-            // 获取canvas
-            const canvas = this.$refs.canvas;
-            // 开启2d的绘制环境
-            const ctx = canvas.getContext("2d");
-            ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+            this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
         }
     }
 })
