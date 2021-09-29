d2591f9e2366699d5b74bca3dff87dd6423eb0db
Jin <88888888@qq.com>
2021-09-29 22:59:21 +0800

    完成清空画布按钮

--- index.js
+++ index.js
@@ -15,14 +15,16 @@ new Vue({
     },
     methods: {
         setCanvasSize(){
-            console.dir(this.$refs.canvasCantainer.clientWidth);
+            // console.dir(this.$refs.canvasCantainer.clientWidth);
             // 获取canvas
             const canvas = this.$refs.canvas
-            canvas.width = this.$refs.canvasCantainer.clientWidth
-            canvas.height = this.$refs.canvasCantainer.clientHeight
+            this.canvasWidth = this.$refs.canvasCantainer.clientWidth
+            this.canvasHeight = this.$refs.canvasCantainer.clientHeight
+            canvas.width = this.canvasWidth
+            canvas.height = this.canvasHeight
         },
         chooseColor(color){
-            console.log(111);
+            // console.log(111);
             this.color=color
         },
         changeSizeIndex(sizeIndex){
@@ -31,39 +33,37 @@ new Vue({
         },
         drawStart(){
             this.touch=true
-            console.log(this.touch)
+            // console.log(this.touch)
         },
         drawEnd(){
             this.touch=false
-            console.log(this.touch)
+            // console.log(this.touch)
             this.lastPoint={x:undefined,y:undefined}
         },
         drawCanvas(event){
-            // 当鼠标按下时
-            if(this.touch===true){
-                // 获取canvas
-                const canvas = this.$refs.canvas
-                // 开启2d的绘制环境
-                ctx = canvas.getContext("2d");
-                // 绘制线条
-                let newPoint= {x:event.targetTouches[0].pageX, y:event.targetTouches[0].pageY}
-                ctx.beginPath();
-                ctx.moveTo(this.lastPoint.x, this.lastPoint.y);
-                ctx.lineWidth = this.size*1.5
-                ctx.strokeStyle  = this.color;
-                ctx.lineCap='round'
-                ctx.lineTo(newPoint.x, newPoint.y);
-                ctx.stroke();
-                ctx.closePath();
-                this.lastPoint = newPoint
-            }
+            // 当手指滑动时
+            // 获取canvas
+            const canvas = this.$refs.canvas
+            // 开启2d的绘制环境
+            ctx = canvas.getContext("2d");
+            // 绘制线条
+            let newPoint= {x:event.targetTouches[0].pageX, y:event.targetTouches[0].pageY}
+            ctx.beginPath();
+            ctx.moveTo(this.lastPoint.x, this.lastPoint.y);
+            ctx.lineWidth = this.size*1.5
+            ctx.strokeStyle  = this.color;
+            ctx.lineCap='round'
+            ctx.lineTo(newPoint.x, newPoint.y);
+            ctx.stroke();
+            ctx.closePath();
+            this.lastPoint = newPoint
         },
-        // clear(){
-        //     // 获取canvas
-        //     const canvas = this.$refs.canvas;
-        //     // 开启2d的绘制环境
-        //     const ctx = canvas.getContext("2d");
-        //     ctx.clearRect(0, 0, 1200, 600);
-        // }
+        clear(){
+            // 获取canvas
+            const canvas = this.$refs.canvas;
+            // 开启2d的绘制环境
+            const ctx = canvas.getContext("2d");
+            ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+        }
     }
 })
